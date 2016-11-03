5f76184d5a279c4a895b394225ff5a97a500c946
Chris Barber <chris.barber@sage.com>
2016-11-03 11:11:15 +0000

    Release v0.0.8

--- s1-docker.rb
+++ s1-docker.rb
@@ -1,7 +1,7 @@
 class S1Docker < Formula
   homepage "https://github.com/andrewjtait/s1-docker"
-  url "https://github.com/andrewjtait/s1-docker/archive/v0.0.7.tar.gz"
-  sha256 "9dd89ff88cb7a3d5ead4b60504dd4e25f08f774f75b3445cb3addaafd1954b3c"
+  url "https://github.com/andrewjtait/s1-docker/archive/v0.0.8.tar.gz"
+  sha256 "e3101f3e54186fdd4c54f654d8f8171d51ecb0221ccab8dc868a4a2a559cc3d4"
 
   def install
     bash_completion.install 's1-docker'
