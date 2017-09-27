59555e619b6ab2f01e2cfdd4d939b1de35a13a00
Andrew Tait <andrew.tait@sage.com>
2017-09-27 16:33:25 +0100

    v0.0.10

--- s1-docker.rb
+++ s1-docker.rb
@@ -1,7 +1,7 @@
 class S1Docker < Formula
   homepage "https://github.com/andrewjtait/s1-docker"
-  url "https://github.com/andrewjtait/s1-docker/archive/v0.0.9.tar.gz"
-  sha256 "939f98f15bb4c604502ec4bf97586c8b49cc7f408acf3a4e854a6eaf73ab8b54"
+  url "https://github.com/andrewjtait/s1-docker/archive/v0.0.10.tar.gz"
+  sha256 "0ee8750efb51d8fef594444e931d52154a4ea9659ca76b16f19671c2ecb967bc"
 
   def install
     bash_completion.install 's1-docker'
